2b8d179c498aad6d0995739f64b6787081a2959b
AyushRajSingh30 <ayushrajsin30@gmail.com>
2025-02-07 00:11:16 +0530

    This is my Controller
    Add GetMessageController with a GET endpoint
    
    Introduce a new API controller `GetMessageController` in the `WebApplication1.Controllers` namespace. This controller is decorated with `[Route("api/[controller]")]` and `[ApiController]` attributes. It includes an action method `GetMessage` that handles HTTP GET requests to `api/GetMessage` and returns a "Hello World!" message as an `Ok` response.

---
+++ WebApplication1/Controllers/GetMessageController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetMessageController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetMessage")]
+        public IActionResult GetMessage()
+        {
+            return Ok("Hello World!");
+        }
+
+    }
+}
